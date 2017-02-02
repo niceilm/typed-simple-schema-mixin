2ba6131bf1adb9d41009a5130dd1c3b97c9fce15
flynn <niceilm@naver.com>
2017-02-02 11:28:23 +0900

    init files

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+interface ValidatedMethodOption {
+  schema?: Object;
+}
+/**
+ * @link https://github.com/rhettlivingston/meteor-simple-schema-mixin
+ */
+declare module 'meteor/rlivingston:simple-schema-mixin' {
+  export let simpleSchemaMixin: any;
+}
